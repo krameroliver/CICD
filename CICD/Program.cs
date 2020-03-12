614528b2353db6c56e569ddbb9496c1de4081668
oliver <krameroliver1@gmail.com>
2020-03-12 13:32:36 +0100

    Initial commit

---
+++ CICD/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using sun.awt;
+
+namespace CICD
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(OSInfo.getOSType());
+        }
+    }
+}
---
+++ CICD_UTs/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CICD;
+using NUnit.Framework;
+
+namespace CICD_UTs
+{
+    [TestFixture]
+    public class WriterTests
+    {
+        [Test]
+        public void WRITER_output_Hello_Test()
+        {
+            WRITER w = new WRITER();
+            w.out_writer="Hello";
+            
+            Assert.True(w.out_writer=="HELLO");
+        }
+    }
+}
